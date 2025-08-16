0b9aad461e281dbb24d886469d418a3272d16654
Sergio Victor Orizano Salvador <sorizano@continental.edu.pe>
2025-08-15 20:44:39 -0500

    modificando main.py

--- main.py
+++ main.py
@@ -10,4 +10,27 @@ ALLOWED_CATEGORIES = [
     "Chocolates", "Caramelos", "Mashmelos", "Galletas", "Salados", "Gomas de mascar"
 ]
 
-st.title("Confitería Dulcino")
+def ensure_dir():
+    os.makedirs(DATA_DIR, exist_ok=True)
+
+def load_df() -> pd.DataFrame:
+    if os.path.exists(CSV_PATH):
+        df = pd.read_csv(CSV_PATH, encoding="utf-8")
+        return df
+    return pd.DataFrame(columns=["nombre", "precio", "categorias", "en_venta", "ts"])
+
+
+#------------------------------------- UI -----------------------------------
+
+st.title("Confitería Dulcino - Registro de productos")
+
+with st.form("form-producto", clear_on_submit=True):
+    col1, col2 = st.columns([2,1])
+    with col1:
+        nombre = st.text_input("Nombre del producto")
+    with col2:
+        precio = st.number_input("Precio (S/)", min_value=0.0, max_value=998.99, step=0.10, format="%.2f")
+    categorias = st.multiselect("Categorias", ALLOWED_CATEGORIES)
+    en_venta_label = st.radio("¿El producto está en venta?", options=["Sí", "No"], horizontal=True)
+
+    submitted = st.form_submit_button("Guardar")
