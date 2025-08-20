8c9eb03d52965518b1d19ac5c8f784959f3a68d3
Sergio Victor Orizano Salvador <sorizano@continental.edu.pe>
2025-08-19 21:02:57 -0500

    agregando reglas

--- main.py
+++ main.py
@@ -47,7 +47,21 @@ def sb_update(id_:int, nombre: str, precio: float, categorias: list, en_venta:bo
 def sb_delete(id_: int):
     supabase.table("products").delete().eq("id", id_).execute()
 
-
+def validar(nombre: str, precio:float, categorias: list[str]) -> str | None:
+    if not nombre or len(nombre.strip()) == 0 or len(nombre.strip()) > 20:
+        return "El nombre es obligatorio y debe de tener <= 20 caracteres."
+    try:
+        p = float(precio)
+    except Exception:
+        return "Por favor verifique el campo precio."
+    if not (0 < p < 999):
+        return "El precio debe ser mayor a 0 y menor a 999"
+    if not categorias:
+        return "Debe elegir al menos una categoría"
+    for c in categorias:
+        if c not in ALLOWED_CATEGORIES:
+            return f"Categoría inválida: {c}"
+    return None
 
 #------------------------------------- UI -----------------------------------
 
@@ -61,3 +75,17 @@ with st.form("form-add", clear_on_submit=True):
     categorias = st.multiselect("Categorias", ALLOWED_CATEGORIES)
     en_venta = st.radio("¿En Venta?",["Sí","No"]) == "Sí"
     submitted = st.form_submit_button("Guardar")
+
+if submitted:
+    err = validar(nombre, precio, categorias)
+    if err:
+        if "precio" in err.lower():
+            st.error("Por favor verifique el campo precio")
+        else:
+            st.error("Lo sentimos no pudo crear este producto")
+        st.info(err)
+    else:
+        sb_insert(nombre.strip(), float(precio), categorias, en_venta)
+        st.success("Felicidades su producto se agregó")
+        st.rerun()
+st.divider()
